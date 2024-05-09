b0659c5dd1f8df5ae39b4d81396a9605b36377d7
Peter Chinman <peterchinman@Peters-MacBook-Pro.local>
2024-05-09 16:36:29 -0400

    preliminary c++ to sqlite3 exploration

---
+++ source/json-to-sql.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "external/nlohmann/json.hpp"
+#include "external/sqlite/sqlite3.h"
+
+int main()
+{
+    sqlite3 *db; // pointer to database connection
+
+    int statusOfOpen = sqlite3_open("wordset_dictionary.sqlite", &db);
+
+    if (statusOfOpen == SQLITE_OK)
+    {
+        std::cout << "Successfully opened the database" << std::endl;
+
+        sqlite3_stmt* myStatement;
+        int statusOfPrep = sqlite3_prepare_v2(db,
+            "CREATE TABLE IF NOT EXISTS Students (ssn INTEGER PRIMARY KEY, first TEXT, last TEXT)",
+            -1, &myStatement, NULL);
+        
+        if (statusOfPrep == SQLITE_OK)
+        {
+            int statusOfStep = sqlite3_step(myStatement);
+
+            if (statusOfStep == SQLITE_DONE)
+            {
+                std::cout << "Successfully created the table" << std::endl;
+            }
+
+            else
+            {
+                std::cout << "Problem creating the table" << std::endl;
+            }
+
+            sqlite3_finalize(myStatement);
+        }
+        else
+        {
+            std::cout << "Problem creating a prepared statement (sqlite3_prepare_v2)" << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Problem creating a prepared statement (sqlite3_open)" << std::endl;
+    }
+
+
+    sqlite3_close(db);
+
+
+    return 0;
+}
